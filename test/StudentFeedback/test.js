5350632dd056810c0d9cf2fd42b64753fa04249b
swonlek <chonglokswen97@hotmail.com>
2018-04-04 11:08:58 +0800

    student feedback testing

--- test/StudentFeedback/test.js
+++ test/StudentFeedback/test.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import sinon from 'sinon';
+import {expect} from 'chai';
 
 // import modules to be tested
 import StudentFeedbackForm from '../../src/components/Student/Feedback';
 
+describe('<StudentFeedbackForm/>', () => {
+	// test initial state
+	it('renders', ()=>{
+		const wrapper = shallow (<StudentFeedbackForm/>);
+		expect(wrapper.contains(<MuiThemeProvider/>)).to.Equal(true)
+		expect(wrapper.find('open').boolean()).to.Equal(false)
+		expect(wrapper.find('pace').text()).to.Equal('0')
+	});	
 
-it('renders', ()=>{
-	const wrapper = shallow (<StudentFeedbackForm>);
-	expect(wrapper.find('open').boolean()).toEqual(false);
+	it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
 })
